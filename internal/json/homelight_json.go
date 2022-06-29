609ef9c834592788d1cd509827c11f555799a453
Moritz Schmidt <moritz.schmidt@loopline-systems.com>
2022-06-29 13:26:09 +0200

    ugh just… hardcode it

--- internal/json/homelight_json.go
+++ internal/json/homelight_json.go
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
 
-//go:build homelight_json
-// +build homelight_json
-
 package json
 
 import "github.com/homelight/json"
